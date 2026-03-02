86f5538190449d4192d35ff0ecadb38a26ffd99c
hi31791423@aliyun.com <doouxiaoyu@163.com>
2026-03-02 09:55:52 +0800

    fix: 导入 SelectableText 修复编译错误

--- lib/features/tray_panel/widgets/tray_panel_unlocked.dart
+++ lib/features/tray_panel/widgets/tray_panel_unlocked.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart' show SelectableText;
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hedge/l10n/generated/app_localizations.dart';
